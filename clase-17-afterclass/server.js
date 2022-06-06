26adfd6f4c28467541b658e7d5602db6a9248bfc
Marcos Villanueva <marcosvillanueva009@gmail.com>
2022-06-06 17:20:25 -0300

    add clase-17-afterclass

---
+++ clase-17-afterclass/server.js
@@ -0,0 +1,28 @@
+// config.js
+
+// export default {
+//     sqlite3: {
+//         client: 'sqlite3',
+//         connection: {
+//             filename: `./DB/ecommerce.sqlite`
+//         },
+//         useNullAsDefault: true
+//     },
+//     mariaDb: {
+//         client: 'mysql',
+//         connection: {
+//             host: 'localhost',
+//             user: 'coderhouse',
+//             password: 'coderhouse',
+//             database: 'coderhouse'
+//         }
+//     }
+// }
+
+// main.js
+
+// import config from './config.js'
+
+// const productosContenedor = new Contenedor(config.mariaDb, 'productos')
+// const mensajesContenedor = new Contenedor(config.sqlite3, 'mensajes')
+
